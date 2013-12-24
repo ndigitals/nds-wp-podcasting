96c1c0c1796c689b0c42267e4525979e3f2b989c
Tim Nolte <tim.nolte@ndigitals.com>
2013-12-24 16:33:48 -0500

    Changed the cover art helper to helper methods to pull episode/series/speaker images. Also added in the initial functions for building an iTunes/Podcast RSS feed.

--- inc/class-nds-wp-podcasting.php
+++ inc/class-nds-wp-podcasting.php
@@ -599,86 +599,127 @@ class NDS_WP_Podcasting
     }
 
     /**
+     * Add iTunes Namespace
+     * add_filter( 'rss2_ns', 'itunes_namespace' );
+     */
+    public function itunes_namespace() {
+        echo 'xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"';
+    }
+
+    /**
+     * Add iTunes Header Metadata
+     * add_filter( 'rss2_head', 'itunes_head' );
+     * TODO: Setup plugins options for these values
+     */
+    public function itunes_head() {
+        ?>
+        <itunes:subtitle></itunes:subtitle>
+        <itunes:author>Daybreak Church</itunes:author>
+        <itunes:summary>Weekly messages from Pastor Wes Dupin and guest speakers.</itunes:summary>
+        <itunes:owner>
+            <itunes:name>Daybreak Church</itunes:name>
+            <itunes:email>webmaster@daybreak.tv</itunes:email>
+        </itunes:owner>
+        <itunes:image href="http://example.com/podcasts/everything/AllAboutEverything.jpg" />
+        <itunes:category text="Technology">
+            <itunes:category text="Gadgets"/>
+        </itunes:category>
+        <?php
+    }
+
+    /**
      * Add iTunes specific RSS feed elements
+     * add_action('rss2_item', 'itunes_attached_audio' );
+     * TODO: Change item author to speaker (not sure if we need an email address).
      */
     public function itunes_attached_audio() {
         global $post;
-        $itunes_keywords = array();
+
         $podcast_audio = get_post_meta( $post->ID, 'nds_wp_podcast_audio', TRUE );
 
-        $attachments = get_posts( array(
-                                       'post_type' => 'attachment',
-                                       'post_mime_type' => 'audio', // if you use videos, change here
-                                       'posts_per_page' => -1,
-                                       'post_parent' => $post->ID,
-                                       'exclude' => get_post_thumbnail_id()
-                                  ) );
-
-        // use the post tags for itunes:keywords
-        $itunes_keywords_arr = get_the_tags();
-        if ( $itunes_keywords_arr ) {
-            foreach( $itunes_keywords_arr as $tag ) {
-                $itunes_keywords[] = $tag->name;
+        if ($podcast_audio)
+        {
+            // Check for then use images from the following sources; episode featured image -> series -> speaker
+            $podcast_image = $this->get_episode_image( $post->ID );
+            if (!$podcast_image)
+            {
+                $podcast_image = $this->get_series_image( $post->ID );
+                if (!$podcast_image)
+                {
+                    $podcast_image = $this->get_speaker_image( $post->ID );
+                }
             }
-        }
 
-        // use the post thumb for itunes:image
-        $post_thumbnail_id = get_post_thumbnail_id( $post->ID );
-        $itunes_image_arr = wp_get_attachment_image_src( $post_thumbnail_id, 'itunes-cover' );
+            // use the post tags for itunes:keywords
+            $itunes_keywords = array();
+            $itunes_keywords_arr = get_the_tags();
+            if ( $itunes_keywords_arr ) {
+                foreach( $itunes_keywords_arr as $tag ) {
+                    $itunes_keywords[] = $tag->name;
+                }
+            }
 
-        if ( $attachments ) {
-            foreach ( $attachments as $att ) {
-                $audio_url = wp_get_attachment_url( $att->ID );
-                $parts = explode( '|', $att->post_content );
-                $headers = get_headers( $audio_url, 1 );
-                $filesize = $headers['Content-Length'];
-                ?>
-                <itunes:author><?php echo get_the_author(); ?></itunes:author>
-                <itunes:subtitle><?php echo $att->post_title; ?></itunes:subtitle>
-                <itunes:summary><?php echo $att->post_excerpt; ?></itunes:summary>
-                <itunes:image href="<?php echo $itunes_image_arr[0]; ?>" />
-                <enclosure url="<?php echo $audio_url; ?>" length="<?php echo $filesize; ?>" type="<?php echo $att->post_mime_type; ?>" />
-                <guid><?php the_permalink(); ?></guid>
-                <itunes:duration><?php echo $att->post_content; ?></itunes:duration>
-                <itunes:keywords><?php echo implode(',', $itunes_keywords); ?></itunes:keywords>
+            $headers = get_headers( $podcast_audio, 1 );
+            $filesize = $headers['Content-Length'];
+            ?>
+            <itunes:author><?php echo get_the_author(); ?></itunes:author>
+            <itunes:subtitle><?php echo $post->post_title; ?></itunes:subtitle>
+            <itunes:summary><?php echo $post->post_excerpt; ?></itunes:summary>
+            <itunes:image href="<?php echo $podcast_image; ?>" />
+            <enclosure url="<?php echo $podcast_audio; ?>" length="<?php echo $filesize; ?>" type="<?php echo $att->post_mime_type; ?>" />
+            <guid><?php the_permalink(); ?></guid>
+            <itunes:duration><?php echo $post->post_content; ?></itunes:duration>
+            <itunes:keywords><?php echo implode(',', $itunes_keywords); ?></itunes:keywords>
             <?php
-            }
         }
     }
 
     /**
-     * Helper function that provides a Podcast image.
+     * Helper function that provides a Podcast episode image.
      */
-    public function get_cover_art( $post_id, $image_size = 'itunes-cover' )
+    public function get_episode_image( $post_id, $image_size = 'itunes-cover' )
     {
-        $speaker_list = wp_get_post_terms( $post_id, 'nds_wp_podcast_speaker' );
+        return wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), $image_size );
+    }
 
-        // Check for then use images from the following sources; episode featured image -> speaker -> series
-        $podcast_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), $image_size );
-        if ( !isset( $podcast_image ) || strlen( $podcast_image ) === 0 || !$podcast_image )
+    /**
+     * Helper function that provides a Podcast series image.
+     */
+    public function get_series_image( $post_id, $image_size = 'itunes-cover' )
+    {
+        $series_list = wp_get_post_terms( $post_id, 'nds_wp_podcast_series' );
+
+        if ( function_exists( 'get_tax_meta' ) )
         {
-            foreach ( $speaker_list as $speaker )
+            foreach ( $series_list as $series )
             {
-                if ( function_exists( 'get_tax_meta' ) && (!isset( $podcast_image ) || !$podcast_image) )
-                {
-                    $podcast_image = get_tax_meta( $speaker->term_id, 'nds_wp_podcast_speaker_image', TRUE );
-                    $podcast_image = (isset($podcast_image['src'])) ? $podcast_image['src'] : false;
-                    if ( !isset( $podcast_image ) || strlen( $podcast_image ) === 0 || !$podcast_image )
-                    {
-                        $series_list = wp_get_post_terms( $post_id, 'nds_wp_podcast_series' );
-                        foreach ( $series_list as $series )
-                        {
-                            $podcast_image = get_tax_meta( $series->term_id, 'nds_wp_podcast_series_image', TRUE );
-                            $podcast_image = (isset($podcast_image['src'])) ? $podcast_image['src'] : false;
-                            break;
-                        }
-                    }
-                    break;
-                }
+                $podcast_image_meta = get_tax_meta( $series->term_id, 'nds_wp_podcast_series_image', TRUE );
+                $podcast_image_id   = ( isset( $podcast_image_meta['id'] ) ) ? $podcast_image_meta['id'] : FALSE;
+                break;
             }
         }
 
-        return $podcast_image;
+        return wp_get_attachment_image_src( $podcast_image_id, $image_size );
+    }
+
+    /**
+     * Helper function that provides a Podcast speaker image.
+     */
+    public function get_speaker_image( $post_id, $image_size = 'itunes-cover' )
+    {
+        $speaker_list  = wp_get_post_terms( $post_id, 'nds_wp_podcast_speaker' );
+
+        if ( function_exists( 'get_tax_meta' ) )
+        {
+            foreach ( $speaker_list as $speaker )
+            {
+                $podcast_image_meta = get_tax_meta( $speaker->term_id, 'nds_wp_podcast_speaker_image', TRUE );
+                $podcast_image_id   = ( isset( $podcast_image_meta['id'] ) ) ? $podcast_image_meta['id'] : FALSE;
+                break;
+            }
+        }
+
+        return wp_get_attachment_image_src( $podcast_image_id, $image_size );
     }
 
 }
